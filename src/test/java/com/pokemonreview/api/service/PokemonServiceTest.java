f0865d99338e43f76bd9894e9f08f9d46eb100fe
manzitresor <manzitre@gmail.com>
2026-01-01 23:50:24 +0200

    chore: add pokemon service test

---
+++ src/test/java/com/pokemonreview/api/service/PokemonServiceTest.java
@@ -0,0 +1,68 @@
+package com.pokemonreview.api.service;
+
+
+import com.pokemonreview.api.dto.PokemonDto;
+import com.pokemonreview.api.dto.PokemonResponse;
+import com.pokemonreview.api.models.Pokemon;
+import com.pokemonreview.api.repository.PokemonRepository;
+import com.pokemonreview.api.service.impl.PokemonServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
+public class PokemonServiceTest {
+    @Mock
+    private PokemonRepository pokemonRepository;
+
+    @InjectMocks
+    private PokemonServiceImpl pokemonService;
+
+    private Pokemon pokemon;
+
+    @BeforeEach
+    void setUp() {
+        pokemon = new Pokemon()
+                .builder()
+                .name("picka")
+                .type("electric")
+                .build();
+    }
+
+    @Test
+    @DisplayName("Should create Pokemon successfully")
+    void PokemonService_CreatePokemon_ReturnPokemonDto() {
+        PokemonDto pokemonDto = PokemonDto.builder().name("picka").type("electric").build();
+
+        when(pokemonRepository.save(Mockito.any(Pokemon.class))).thenReturn(pokemon);
+        PokemonDto result = pokemonService.createPokemon(pokemonDto);
+
+        Assertions.assertEquals(pokemonDto.getName(), result.getName());
+        Assertions.assertEquals(pokemonDto.getType(), result.getType());
+        Assertions.assertNotNull(result);
+
+        verify(pokemonRepository,times(1)).save(Mockito.any(Pokemon.class));
+    }
+
+    @Test
+    @DisplayName("Should get all Pokemon successfully")
+    void PokemonService_GetAll_ReturnResponseDto() {
+        Page<Pokemon> pokemonPage = Mockito.mock(Page.class);
+
+        when(pokemonRepository.findAll(Mockito.any(Pageable.class))).thenReturn(pokemonPage);
+
+        PokemonResponse savedPokemon = pokemonService.getAllPokemon(1,10);
+        Assertions.assertNotNull(savedPokemon);
+        verify(pokemonRepository,times(1)).findAll(Mockito.any(Pageable.class));
+    }
+}
